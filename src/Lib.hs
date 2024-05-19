38a223817543e082eca2040225ce6189fda0774b
Endi Sukaj <endisukaj@gmail.com>
2024-05-19 12:37:33 +0200

    Initial commit

---
+++ src/Lib.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Lib
+  ( search,
+  )
+where
+
+import Data.Aeson (decode, encode, object, (.=))
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import RIO.ByteString
+import Types (SerperResult)
+
+search :: ByteString -> String -> IO (Maybe SerperResult)
+search apiKey query = do
+  manager <- newManager tlsManagerSettings
+  let requestObject = object ["q" .= query]
+  initialRequest <- parseRequest "https://google.serper.dev/search"
+  let request = initialRequest {method = "POST", requestBody = RequestBodyLBS $ encode requestObject, requestHeaders = [("X-API-KEY", apiKey), ("Content-Type", "application/json")]}
+  response <- httpLbs request manager
+  print response
+  return $ decode $ responseBody response
---
+++ src/Types.hs
@@ -0,0 +1,87 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Types (SerperResult, OrganicGoogleSearchResult, SiteLink, PeopleAlsoAskResult, RelatedSearch) where
+
+import Data.Aeson
+import Data.Aeson.Types (prependFailure, typeMismatch)
+import GHC.Generics (Generic)
+
+data SerperResult = SerperResult
+  { organic :: [OrganicGoogleSearchResult],
+    peopleAlsoAsk :: [PeopleAlsoAskResult],
+    relatedSearches :: [RelatedSearch]
+  }
+  deriving (Generic, Show)
+
+instance FromJSON SerperResult
+
+data OrganicGoogleSearchResult = OrganicGoogleSearchResult
+  { organicGoogleSearchResultTitle :: String,
+    organicGoogleSearchResultLink :: String,
+    organicGoogleSearchResultSnippet :: String,
+    position :: Int,
+    sitelinks :: Maybe [SiteLink],
+    date :: Maybe String
+  }
+  deriving (Show)
+
+instance FromJSON OrganicGoogleSearchResult where
+  parseJSON (Object v) =
+    OrganicGoogleSearchResult
+      <$> v
+        .: "title"
+      <*> v
+        .: "link"
+      <*> v
+        .: "snippet"
+      <*> v
+        .: "position"
+      <*> v
+        .:? "sitelinks"
+      <*> v
+        .:? "date"
+  parseJSON invalid = prependFailure "parsing OrganicGoogleSearchResult failed, " (typeMismatch "Object" invalid)
+
+data SiteLink = SiteLink
+  { siteLinkTitle :: String,
+    siteLinkLink :: String
+  }
+  deriving (Show)
+
+instance FromJSON SiteLink where
+  parseJSON (Object v) =
+    SiteLink
+      <$> v
+        .: "title"
+      <*> v
+        .: "link"
+  parseJSON invalid = prependFailure "parsing SiteLink failed, " (typeMismatch "Object" invalid)
+
+data PeopleAlsoAskResult = PeopleAlsoAskResult
+  { question :: String,
+    peopleAlsoAskResultSnippet :: String,
+    peopleAlsoAskResultTitle :: String,
+    peopleAlsoAskResultLink :: String
+  }
+  deriving (Show)
+
+instance FromJSON PeopleAlsoAskResult where
+  parseJSON (Object v) =
+    PeopleAlsoAskResult
+      <$> v
+        .: "question"
+      <*> v
+        .: "snippet"
+      <*> v
+        .: "title"
+      <*> v
+        .: "link"
+  parseJSON invalid = prependFailure "parsing PeopleAlsoAskResult failed, " (typeMismatch "Object" invalid)
+
+newtype RelatedSearch = RelatedSearch
+  { query :: String
+  }
+  deriving (Generic, Show)
+
+instance FromJSON RelatedSearch
